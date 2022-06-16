dd935dcac88e99d870aaeb5ce8cbc840e1853eab
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-06-16 12:08:47 +0900

    feat: temp state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,17 @@ import TempInput from "./TempInput";
 
 const App = () => {
    
-
+    const [temp, setTemp] = useState();
 
     return(
         <div className="wrapper">
             <div className="ui segment">
                 <div className="header">
                     <h1>Temperature Converter</h1>
-                    <TempInput />
+                    <TempInput 
+                    temp={temp}
+                    setTemp={setTemp}
+                    />
                 </div>
             </div>
         </div>
