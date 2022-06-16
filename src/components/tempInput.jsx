159020384ec246a97210574cff0d55939ace612c
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-06-16 12:10:17 +0900

    update: new form tag structure

--- src/components/tempInput.jsx
+++ src/components/tempInput.jsx
@@ -3,11 +3,35 @@ import React, { useState, useEffect } from "react";
 import DropDown from "./DropDown";
 import "./tempapp.css"
 
-const TempInput = () => {
+const TempInput = (props) => {
    
+    const {temp, setTemp} = props; 
+    const temperatures = {
+        fahrenheit: "Fahrenheit",
+        celcius: "Celcius"
+    };
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setTemp(e.target.value);
+    };
+
     return(
-        <>
-        </>
+        <div>
+             <form id="tempCalc">
+                
+                <input type="number" className="temp" />
+
+                <select id="temps ui segment" value={temp} onChange={handleChange} >
+                    <option value="Celcius">{temperatures.celcius}</option>
+                    <option value="Fahrenheit">{temperatures.fahrenheit}</option>
+                </select> <br />
+
+                <input type="submit" className="temp ui segment" /> <br />
+                <span id="result-container"></span>
+            </form>
+
+        </div>
     )
 }
 
