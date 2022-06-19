494e0dc0db67b8210e3bd24f958f304c2a7cc749
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-06-19 21:02:54 +0900

    feat: toggleswitch component

---
+++ src/components/ToggleSwitch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactSwitch from "react-switch";
+
+const ToggleSwitch = () => {
+    return (
+        <div>
+            <ReactSwitch />
+        </div>
+    )
+};
+
+export default ToggleSwitch;
